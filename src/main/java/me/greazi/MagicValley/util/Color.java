24a8197450b9e63e9e2bd6ce98c9e95f94e78a82
Greazi <n.spel.game@gmail.com>
2021-04-18 11:05:07 +0200

    Removed color util

--- src/main/java/me/greazi/MagicValley/util/Color.java
+++
@@ -1,9 +0,0 @@
-package me.greazi.MagicValley.util;
-
-import org.bukkit.ChatColor;
-
-public class Color {
-    public static String color(String msg) {
-        return ChatColor.translateAlternateColorCodes('&', msg);
-    }
-}
