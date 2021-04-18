cd462f49fd7abdd0399ebda4af96612d16ebbedc
Greazi <n.spel.game@gmail.com>
2021-04-18 11:16:27 +0200

    Main command

---
+++ src/main/java/me/greazi/magicvalley/commands/MVplCommand.java
@@ -0,0 +1,64 @@
+package me.greazi.magicvalley.commands;
+
+import me.greazi.magicvalley.config.Config;
+import me.greazi.magicvalley.locale.LocaleLoader;
+import me.greazi.magicvalley.MVpl;
+import me.greazi.magicvalley.util.Permissions;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class MVplCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
+        switch (args.length) {
+            case 0:
+                if (!Permissions.mvplDescription(sender)) {
+                    sender.sendMessage(command.getPermissionMessage());
+                    return true;
+                }
+
+                String description = LocaleLoader.getString("mvpl.Description");
+                String[] mcSplit = description.split(",");
+                sender.sendMessage(mcSplit);
+                sender.sendMessage(LocaleLoader.getString("mvpl.Description.Dev"));
+
+                if (Config.getInstance().getDonateMessageEnabled()) {
+                    sender.sendMessage(LocaleLoader.getString("MOTD.Donate"));
+                    sender.sendMessage(ChatColor.GOLD + " - " + ChatColor.GREEN + "greazi@gmail.com" + ChatColor.GOLD + " Paypal");
+                }
+
+                if (Permissions.showversion(sender)) {
+                    sender.sendMessage(LocaleLoader.getString("MOTD.Version", MVpl.p.getDescription().getVersion()));
+                }
+
+//                mcMMO.getHolidayManager().anniversaryCheck(sender);
+                return true;
+
+            case 1:
+                if (args[0].equalsIgnoreCase("?") || args[0].equalsIgnoreCase("help") || args[0].equalsIgnoreCase("commands")) {
+                    if (!Permissions.mvplHelp(sender)) {
+                        sender.sendMessage(command.getPermissionMessage());
+                        return true;
+                    }
+
+                    sender.sendMessage(LocaleLoader.getString("Commands.mv.Header"));
+                    displayGeneralCommands(sender);
+                }
+                return true;
+
+            default:
+                return false;
+        }
+    }
+
+    private void displayGeneralCommands(CommandSender sender) {
+        sender.sendMessage(ChatColor.DARK_AQUA + " /mcstats " + LocaleLoader.getString("Commands.Stats"));
+        sender.sendMessage(ChatColor.DARK_AQUA + " /<skill>" + LocaleLoader.getString("Commands.SkillInfo"));
+        sender.sendMessage(ChatColor.DARK_AQUA + " /mctop " + LocaleLoader.getString("Commands.Leaderboards"));
+    }
+
+
+}
