68d4a9f7962c3b07dc5608e8ee6e868b663dfa6d
Greazi <n.spel.game@gmail.com>
2021-04-18 11:16:17 +0200

    Added permmissions handler for commands

---
+++ src/main/java/me/greazi/magicvalley/util/Permissions.java
@@ -0,0 +1,34 @@
+package me.greazi.magicvalley.util;
+
+import org.bukkit.permissions.Permissible;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionDefault;
+import org.bukkit.plugin.PluginManager;
+
+public final class Permissions {
+    private Permissions() {}
+
+    /*
+     * GENERAL
+     */
+    public static boolean motd(Permissible permissible) { return permissible.hasPermission("mvpl.motd"); }
+    public static boolean showversion(Permissible permissible) { return permissible.hasPermission("mvpl.showversion"); }
+
+    /*
+     * COMMANDS
+     */
+    public static boolean mvinfo(Permissible permissible) { return permissible.hasPermission("mvpl.commands.mvplinfo"); }
+
+    public static boolean mvplDescription(Permissible permissible) { return permissible.hasPermission("mvpl.commands.mvpl.description"); }
+    public static boolean mvplHelp(Permissible permissible) { return permissible.hasPermission("mvpl.commands.mvpl.help"); }
+
+    public static boolean reloadlocale(Permissible permissible) { return permissible.hasPermission("mvpl.commands.reloadlocale"); }
+    public static boolean mvtest(Permissible permissible) { return permissible.hasPermission("mvpl.commands.mvtest"); }
+
+    private static void addDynamicPermission(String permissionName, PermissionDefault permissionDefault, PluginManager pluginManager) {
+        Permission permission = new Permission(permissionName);
+        permission.setDefault(permissionDefault);
+        pluginManager.addPermission(permission);
+    }
+
+}
