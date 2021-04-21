7a05217c3877745de03554d6dc81ed5869051dfb
Greazi <n.spel.game@gmail.com>
2021-04-21 10:28:58 +0200

    First Help page (test)

--- src/main/java/me/greazi/magicvalley/commands/test/MVTest.java
+++ src/main/java/me/greazi/magicvalley/commands/test/MVTest.java
@@ -40,6 +40,20 @@ public class MVTest implements CommandExecutor {
                 }
                 return true;
 
+            case 2:
+                if (args[0].equalsIgnoreCase("?") || args[0].equalsIgnoreCase("help") || args[0].equalsIgnoreCase("commands")) {
+                    if (args[1].equalsIgnoreCase("2")) {
+                        if (!Permissions.mvplHelp(sender)) {
+                            sender.sendMessage(command.getPermissionMessage());
+                            return true;
+                        }
+
+                        sender.sendMessage(getHelpHeader());
+                        sender.sendMessage(ColorUtil.addColors("&6This is page 2&c&l!!!!!!"));
+                        footerHelpP2(sender);
+                    }
+                }
+
             default:
                 return false;
         }
@@ -48,13 +62,6 @@ public class MVTest implements CommandExecutor {
     private void displayGeneralCommands(CommandSender sender) {
         sender.sendMessage(ColorUtil.addColors("   &6&l- &a/mvpl " + LocaleLoader.getString("Commands.Description.mvpl")));
         sender.sendMessage(ColorUtil.addColors("   &6&l- &a/mvtest " + LocaleLoader.getString("Commands.Description.mvtest")));
-    }
-
-    private void NextPageComponent() {
-        TextComponent subComponent = new TextComponent( ">>" );
-        subComponent.setColor( ChatColor.AQUA );
-        subComponent.setHoverEvent( new HoverEvent( HoverEvent.Action.SHOW_TEXT, new ComponentBuilder( "Click me!" ).create() ) );
-        subComponent.setClickEvent( new ClickEvent( ClickEvent.Action.RUN_COMMAND,"mvtest help 2"));
     }
 
     protected String[] getHelpHeader() {
@@ -71,16 +78,42 @@ public class MVTest implements CommandExecutor {
     }
 
     private void footerHelpP1(CommandSender sender) {
-        TextComponent firstComponent = new TextComponent(ColorUtil.addColors("&8&m---------------------- &7« "));
-        firstComponent.setColor( ChatColor.GOLD );
+        TextComponent firstComponent = new TextComponent(ColorUtil.addColors("&8&m---------------------- &7«"));
+        firstComponent.setColor( ChatColor.DARK_GRAY );
 
-        TextComponent subComponent = new TextComponent( "Maybe u r noob?" );
-        subComponent.setColor( ChatColor.AQUA );
-        subComponent.setHoverEvent( new HoverEvent( HoverEvent.Action.SHOW_TEXT, new ComponentBuilder( "Click me!" ).create() ) );
-        subComponent.setClickEvent( new ClickEvent( ClickEvent.Action.OPEN_URL, "https://www.spigotmc.org/wiki/the-chat-component-api/" ) );
+        TextComponent subComponent1 = new TextComponent( "1/2" );
+        subComponent1.setColor( ChatColor.GRAY );
+        subComponent1.setHoverEvent( new HoverEvent( HoverEvent.Action.SHOW_TEXT, new ComponentBuilder( ColorUtil.addColors("&7You can also navigate using the hidden '/mvplv help <page>' command." )).create() ) );
+        subComponent1.setClickEvent( new ClickEvent( ClickEvent.Action.OPEN_URL, "" ) );
 
-        firstComponent.addExtra( subComponent );
-        firstComponent.addExtra( " &8&m----------------------" );
+        TextComponent subComponent2 = new TextComponent( " » " );
+        subComponent2.setColor( ChatColor.GRAY );
+        subComponent2.setHoverEvent( new HoverEvent( HoverEvent.Action.SHOW_TEXT, new ComponentBuilder( ColorUtil.addColors("&7Go to page 2" )).create() ) );
+        subComponent2.setClickEvent( new ClickEvent( ClickEvent.Action.RUN_COMMAND, "/mvtest ? 2" ) );
+
+        firstComponent.addExtra( subComponent1 );
+        firstComponent.addExtra( subComponent2 );
+        firstComponent.addExtra( ColorUtil.addColors("&8&m----------------------"));
+        sender.spigot().sendMessage( firstComponent );
+    }
+
+    private void footerHelpP2(CommandSender sender) {
+        TextComponent firstComponent = new TextComponent(ColorUtil.addColors("&8&m---------------------- &7«"));
+        firstComponent.setColor( ChatColor.DARK_GRAY );
+
+        TextComponent subComponent1 = new TextComponent( "1/2" );
+        subComponent1.setColor( ChatColor.GRAY );
+        subComponent1.setHoverEvent( new HoverEvent( HoverEvent.Action.SHOW_TEXT, new ComponentBuilder( ColorUtil.addColors("&7You can also navigate using the hidden '/mvplv help <page>' command." )).create() ) );
+        subComponent1.setClickEvent( new ClickEvent( ClickEvent.Action.OPEN_URL, "" ) );
+
+        TextComponent subComponent2 = new TextComponent( " &7» " );
+        subComponent2.setColor( ChatColor.GRAY );
+        subComponent2.setHoverEvent( new HoverEvent( HoverEvent.Action.SHOW_TEXT, new ComponentBuilder( ColorUtil.addColors("&7Go to page 2" )).create() ) );
+        subComponent2.setClickEvent( new ClickEvent( ClickEvent.Action.RUN_COMMAND, "mvtest ?" ) );
+
+        firstComponent.addExtra( subComponent1 );
+        firstComponent.addExtra( subComponent2 );
+        firstComponent.addExtra( ColorUtil.addColors("&8&m----------------------"));
         sender.spigot().sendMessage( firstComponent );
     }
 }
