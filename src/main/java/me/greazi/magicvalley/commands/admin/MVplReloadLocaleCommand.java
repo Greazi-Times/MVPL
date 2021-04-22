c8b35c56dc0a032a6b754a02667caf769234e4a8
Greazi <n.spel.game@gmail.com>
2021-04-22 20:53:32 +0200

    Added Locale reload command

--- src/main/java/me/greazi/magicvalley/commands/admin/MVplReloadLocaleCommand.java
+++ src/main/java/me/greazi/magicvalley/commands/admin/MVplReloadLocaleCommand.java
@@ -1,16 +1,17 @@
 package me.greazi.magicvalley.commands.admin;
 
+import me.greazi.magicvalley.MVpl;
 import me.greazi.magicvalley.locale.LocaleLoader;
 import me.greazi.magicvalley.util.Permissions;
+import me.greazi.magicvalley.util.text.ColorUtil;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 
-/**
- * @author Mark Vainomaa
- */
 public final class MVplReloadLocaleCommand implements CommandExecutor {
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         if (args.length == 0) {
@@ -19,9 +20,18 @@ public final class MVplReloadLocaleCommand implements CommandExecutor {
                 return true;
             }
 
+            sender.sendMessage(ColorUtil.addColors("&aReloading Locale...."));
             LocaleLoader.reloadLocale();
-            sender.sendMessage(LocaleLoader.getString("Locale.Reloaded"));
 
+            new BukkitRunnable(){
+
+                @Override
+                public void run() {
+                    sender.sendMessage(LocaleLoader.getString("Locale.Reloaded"));
+
+                }
+
+            }.runTaskLater(MVpl.p, 20 * 4);
             return true;
         }
         return false;
