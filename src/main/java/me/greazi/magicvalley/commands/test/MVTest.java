381da6c3a7df224df7c42aba0dfd47ac3170f313
Greazi <n.spel.game@gmail.com>
2021-04-25 14:59:07 +0200

    Added copyright and fix on help command

--- src/main/java/me/greazi/magicvalley/commands/test/MVTest.java
+++ src/main/java/me/greazi/magicvalley/commands/test/MVTest.java
@@ -14,6 +14,15 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
+/*
+ *
+ * Copyright © Greazi 2020 | All Rights Reserved.
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ * Written by Greazi
+ *
+ */
+
 public class MVTest implements CommandExecutor {
 
     @Override
@@ -34,7 +43,7 @@ public class MVTest implements CommandExecutor {
                     }
 
                     sender.sendMessage(getHelpHeader());
-                    displayGeneralCommands(sender);
+                    displayPage1(sender);
                     footerHelpP1(sender);
 
                 }
@@ -42,26 +51,35 @@ public class MVTest implements CommandExecutor {
 
             case 2:
                 if (args[0].equalsIgnoreCase("?") || args[0].equalsIgnoreCase("help") || args[0].equalsIgnoreCase("commands")) {
+                    if (!Permissions.mvplHelp(sender)) {
+                        sender.sendMessage(command.getPermissionMessage());
+                        return true;
+                    }
+                    if (args[1].equalsIgnoreCase("1")) {
+                        sender.sendMessage(getHelpHeader());
+                        displayPage1(sender);
+                        footerHelpP1(sender);
+                        return true;
+                    }
                     if (args[1].equalsIgnoreCase("2")) {
-                        if (!Permissions.mvplHelp(sender)) {
-                            sender.sendMessage(command.getPermissionMessage());
-                            return true;
-                        }
-
                         sender.sendMessage(getHelpHeader());
                         sender.sendMessage(ColorUtil.addColors("&6This is page 2&c&l!!!!!!"));
                         footerHelpP2(sender);
+                        return true;
+                    } else {
+                        return false;
                     }
                 }
+                return true;
 
             default:
                 return false;
         }
     }
 
-    private void displayGeneralCommands(CommandSender sender) {
-        sender.sendMessage(ColorUtil.addColors("   &6&l- &a/mvpl " + LocaleLoader.getString("Commands.Description.mvpl")));
-        sender.sendMessage(ColorUtil.addColors("   &6&l- &a/mvtest " + LocaleLoader.getString("Commands.Description.mvtest")));
+    private void displayPage1(CommandSender sender) {
+        sender.sendMessage(ColorUtil.addColors("   &6&l- &a/mvpl     &8|" + LocaleLoader.getString("Commands.Description.mvpl")));
+        sender.sendMessage(ColorUtil.addColors("   &6&l- &a/mvtest   &8|" + LocaleLoader.getString("Commands.Description.mvtest")));
     }
 
     protected String[] getHelpHeader() {
@@ -78,15 +96,16 @@ public class MVTest implements CommandExecutor {
     }
 
     private void footerHelpP1(CommandSender sender) {
-        TextComponent firstComponent = new TextComponent(ColorUtil.addColors("&8&m---------------------- &7«"));
+        TextComponent firstComponent = new TextComponent(ColorUtil.addColors("&8&m----------------------&7 « "));
         firstComponent.setColor( ChatColor.DARK_GRAY );
 
         TextComponent subComponent1 = new TextComponent( "1/2" );
         subComponent1.setColor( ChatColor.GRAY );
-        subComponent1.setHoverEvent( new HoverEvent( HoverEvent.Action.SHOW_TEXT, new ComponentBuilder( ColorUtil.addColors("&7You can also navigate using the hidden '/mvplv help <page>' command." )).create() ) );
+        subComponent1.setHoverEvent( new HoverEvent( HoverEvent.Action.SHOW_TEXT, new ComponentBuilder( ColorUtil.addColors( hoverPage() )).create() ) );
+
         subComponent1.setClickEvent( new ClickEvent( ClickEvent.Action.OPEN_URL, "" ) );
 
-        TextComponent subComponent2 = new TextComponent( " » " );
+        TextComponent subComponent2 = new TextComponent( ColorUtil.addColors( " &6» " ) );
         subComponent2.setColor( ChatColor.GRAY );
         subComponent2.setHoverEvent( new HoverEvent( HoverEvent.Action.SHOW_TEXT, new ComponentBuilder( ColorUtil.addColors("&7Go to page 2" )).create() ) );
         subComponent2.setClickEvent( new ClickEvent( ClickEvent.Action.RUN_COMMAND, "/mvtest ? 2" ) );
@@ -98,22 +117,23 @@ public class MVTest implements CommandExecutor {
     }
 
     private void footerHelpP2(CommandSender sender) {
-        TextComponent firstComponent = new TextComponent(ColorUtil.addColors("&8&m---------------------- &7«"));
+        TextComponent firstComponent = new TextComponent(ColorUtil.addColors("&8&m----------------------&6 « "));
         firstComponent.setColor( ChatColor.DARK_GRAY );
 
-        TextComponent subComponent1 = new TextComponent( "1/2" );
+        TextComponent subComponent1 = new TextComponent( "2/2" );
         subComponent1.setColor( ChatColor.GRAY );
-        subComponent1.setHoverEvent( new HoverEvent( HoverEvent.Action.SHOW_TEXT, new ComponentBuilder( ColorUtil.addColors("&7You can also navigate using the hidden '/mvplv help <page>' command." )).create() ) );
+
+        subComponent1.setHoverEvent( new HoverEvent( HoverEvent.Action.SHOW_TEXT, new ComponentBuilder( hoverPage() ).create() ) );
         subComponent1.setClickEvent( new ClickEvent( ClickEvent.Action.OPEN_URL, "" ) );
 
-        TextComponent subComponent2 = new TextComponent( " &7» " );
-        subComponent2.setColor( ChatColor.GRAY );
-        subComponent2.setHoverEvent( new HoverEvent( HoverEvent.Action.SHOW_TEXT, new ComponentBuilder( ColorUtil.addColors("&7Go to page 2" )).create() ) );
-        subComponent2.setClickEvent( new ClickEvent( ClickEvent.Action.RUN_COMMAND, "mvtest ?" ) );
-
         firstComponent.addExtra( subComponent1 );
-        firstComponent.addExtra( subComponent2 );
-        firstComponent.addExtra( ColorUtil.addColors("&8&m----------------------"));
+        firstComponent.addExtra( ColorUtil.addColors(" &7» &8&m----------------------"));
         sender.spigot().sendMessage( firstComponent );
     }
+
+    private static String  hoverPage(){
+        return  "§7You can also navigate using the\n" +
+                "§7hidden '/mvplv help <page>' command.";
+    }
+
 }
